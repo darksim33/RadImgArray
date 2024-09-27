5108c5011aeae87784f7a6300f17c162669f7a2a
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-09-27 13:54:15 +0200

    feat: add file in out fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,25 @@
 import pytest
 from pathlib import Path
 
+
 @pytest.fixture
 def root():
     return Path(__file__).parent.parent
 
+
 @pytest.fixture
 def nifti_file(root):
     return root / "tests" / ".assets" / "test_img.nii.gz"
+
+
+@pytest.fixture
+def nifti_out_file(root):
+    file = root / "tests" / "out_img.nii.gz"
+    yield file
+    if file.is_file():
+        file.unlink()
+
+
+@pytest.fixture
+def dicom_folder(root):
+    return root / "tests" / ".assets" / "dicom"
