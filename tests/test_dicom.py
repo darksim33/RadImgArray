ab7d090e210fc7fa056fbd05f8ab0d352672862b
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-09-27 13:57:35 +0200

    feat: add dicom load test

--- tests/test_dicom.py
+++ tests/test_dicom.py
@@ -1,8 +1,9 @@
-from pathlib import Path
+import numpy as np
 
 from radimgarray import RadImgArray
 
 
-def test_load_dicom():
-    folder = Path(r"E:\data\testdata\240926_NNLSPhantom")
-    rad_img = RadImgArray().load(folder)
+def test_load_dicom(dicom_folder):
+    rad_img = RadImgArray(dicom_folder)
+    np_array = np.random.rand(176, 176, 15, 3)
+    assert rad_img.shape == np_array.shape
