19c514b94c32995480a2f893519a3bbcce2b6c94
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-09-27 13:57:52 +0200

    feat: rework test and add save test

--- tests/test_nifti.py
+++ tests/test_nifti.py
@@ -1,9 +1,18 @@
 from radimgarray import RadImgArray
-import numpy as np
-from pathlib import Path
+import nibabel as nib
+
+from test_rad_img_array import behaves_like_ndarray
+
 
 def test_from_nii(nifti_file):
-    rad_img = RadImgArray()
-    rad_img.load(nifti_file)
-    assert rad_img.shape == (178, 178, 3, 0)
+    rad_img = RadImgArray(nifti_file)
 
+    nii = nib.load(nifti_file)
+    np_array = nii.get_fdata()
+    behaves_like_ndarray(rad_img, np_array)
+
+
+def test_save_from_nii(nifti_file, nifti_out_file):
+    rad_img = RadImgArray(nifti_file)
+    rad_img.save(nifti_out_file)
+    assert nifti_out_file.exists()
