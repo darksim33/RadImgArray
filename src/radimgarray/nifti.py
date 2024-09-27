b49153fa229316ea6ae1df680d71ddf55ff1e163
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-09-27 13:57:08 +0200

    docs fix: add method documentation and fix for dicom2nifti

--- src/radimgarray/nifti.py
+++ src/radimgarray/nifti.py
@@ -1,15 +1,36 @@
+import warnings
 import numpy as np
 import nibabel as nib
 from pathlib import Path
 
 
 def load(file: Path):
+    """
+    Load a nifti file
+    Args:
+        file:
+
+    Returns:
+        :nifti1.Nifti1Image | :nifti2.Nifti2Image
+    """
     return nib.load(file)
 
 
-def update(data: np.ndarray | list, nii: nib.Nifti1Image | nib.Nifti2Image, **kwargs):
+def update(
+    array: np.ndarray | list, nii: nib.Nifti1Image | nib.Nifti2Image, **kwargs
+) -> nib.nifti1.Nifti1Image | nib.nifti2.Nifti2Image:
+    """
+    Update nifti data
+    Args:
+        array:
+        nii:
+        **kwargs:
+
+    Returns:
+
+    """
     if nii is not None:
-        return nib.Nifti1Image(data, nii.affine, nii.header)
+        return nib.Nifti1Image(array, nii.affine, nii.header)
     else:
         affine = kwargs.get("affine", np.eye(4))
         header = kwargs.get("header", nib.Nifti1Header())
@@ -21,12 +42,32 @@ def update(data: np.ndarray | list, nii: nib.Nifti1Image | nib.Nifti2Image, **kw
         if kwargs.get("nifti_dtype", "Nifti1") == "Nifti2":
             if isinstance(header, nib.nifti1.Nifti1Header):
                 header = nib.nifti2.Nifti2Header.from_header(header)
-            return nib.Nifti2Image(data, affine, header)
+            return nib.Nifti2Image(array, affine, header)
         else:
-            return nib.Nifti1Image(data, affine, header)
+            return nib.Nifti1Image(array, affine, header)
 
 
-def save(nii: nib.Nifti1Image, path: Path, do_zip: bool = True):
+def save(
+    array: np.ndarray,
+    path: Path,
+    nii: nib.Nifti1Image | None = None,
+    do_zip: bool = True,
+):
+    """
+    Save array to nifti file
+    Args:
+        array: numpy array
+        nii: nibabel image type
+        path: path to save nifti file
+        do_zip: whether to zip the nifti file
+    """
+    nii_array = nii.get_fdata()
+    if nii_array.shape != array.shape:
+        warnings.warn(
+            "Array dimensions have changed since import. Updating nifti header."
+        )
+        nii = update(array, nii)
+
     if do_zip and path.suffix != ".gz":
         nib.save(nii, path.with_suffix(".nii.gz"))
     else:
@@ -34,7 +75,7 @@ def save(nii: nib.Nifti1Image, path: Path, do_zip: bool = True):
 
 
 def check_for_nifti(file: Path):
-    if file.is_file():
+    if not file.is_dir():
         if file.suffix == ".nii":
             return True
         elif file.suffix == ".gz":
@@ -42,6 +83,7 @@ def check_for_nifti(file: Path):
                 return True
     else:
         return False
+
 
 def dicom_header_to_nifti_header(dicom_headers):
     nifti_header = nib.Nifti1Header()
@@ -52,8 +94,8 @@ def dicom_header_to_nifti_header(dicom_headers):
                     nifti_header[key] = value
                 except KeyError:
                     pass
-            elif header[key] != value:
-                if type(header[key]) != list:
-                    nifti_header[key] = list(header[key])
-                header[key].append(value)
+            elif nifti_header[key] != value:
+                if type(nifti_header[key]) != list:
+                    nifti_header[key] = list(nifti_header[key])
+                nifti_header[key].append(value)
     return nifti_header
