f446060ee0bd272ce6eb325458cab7174f10b9b5
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-10-04 15:47:41 +0200

    feat: add more elaborated testing

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,14 +3,23 @@ from radimgarray import tools
 from radimgarray import RadImgArray
 
 
+def get_random_even_int():
+    while True:
+        n = np.random.randint(1, 64)
+        if n % 2 == 0:
+            return n
+
+
 def test_zero_pad_to_square():
     while True:
-        x = np.random.randint(1, 64)
-        y = np.random.randint(1, 64)
-        z = np.random.randint(1, 64)
-        array = np.ones((x, y, z))
+        x = get_random_even_int()
+        y = get_random_even_int()
+        z = get_random_even_int()
+        array = np.random.rand(x, y, z)
         if not x == y:
             break
     new_array = tools.zero_pad_to_square(array)
     assert not new_array.shape == array.shape
-    RadImgArray(new_array)
+
+    new_array_fft = tools.zero_pad_to_square(array, k_space=True)
+    assert not new_array_fft.shape == array.shape
