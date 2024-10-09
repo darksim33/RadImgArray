377a233f98b480c0ddc51dcdfdbbc55ab90b0247
thiel <57491748+darksim33@users.noreply.github.com>
2024-10-09 17:36:13 +0200

    feat: init get_mean_signal

--- src/radimgarray/tools.py
+++ src/radimgarray/tools.py
@@ -30,3 +30,27 @@ def zero_pad_to_square(array):
             return type(array)(new_array)
         else:
             return new_array
+
+
+def get_mean_signal(img: RadImgArray | np.ndarray, seg: SegImageArray, value: int):
+    """
+    Get the mean signal of a specific segmentation.
+    Args:
+        img: RadImgArray | np.ndarray: The image data.
+        seg: SegImageArray: The segmentation data
+        value: int: The segmentation value to find.
+    Returns:
+        float: The mean signal of the segmentation value.
+    Raises:
+        ValueError: If the segmentation value is not found in the array.
+    """
+
+    if img.shape[:-1] != seg.shape[:-1]:
+        raise ValueError("Image and segmentation shape do not match")
+
+    if value in seg.seg_values:
+        array = np.where(seg == value)
+        img[not array] = np.nan
+        return np.nanmean(img, axis=3)
+    else:
+        raise ValueError(f"Segmentation value {value} not found in array")
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,16 +1,31 @@
 import numpy as np
 from radimgarray import tools
-from radimgarray import RadImgArray
+from radimgarray import RadImgArray, SegImageArray
 
 
 def test_zero_pad_to_square():
     while True:
-        x = np.random.randint(1, 64)
-        y = np.random.randint(1, 64)
-        z = np.random.randint(1, 64)
+        x = np.random.randint(1, 64) * 2
+        y = np.random.randint(1, 64) * 2
+        z = np.random.randint(1, 64) * 2
         array = np.ones((x, y, z))
         if not x == y:
             break
     new_array = tools.zero_pad_to_square(array)
     assert not new_array.shape == array.shape
     RadImgArray(new_array)
+
+
+def test_get_mean_signal(nifti_file, nifti_seg_file):
+    cube_size = np.random.randint(20, 64)
+    size = np.random.randint(10, 32) * 2 + cube_size * 2
+    array = np.zeros((size, size, size, 1))
+    array[
+        cube_size:-cube_size,
+        cube_size:-cube_size,
+        cube_size:-cube_size,
+    ] = 1
+    seg = SegImageArray(array)
+    img = np.ones((size, size, size, 16))
+    mean = tools.get_mean_signal(img, seg, 1)
+    assert mean.max == 1
